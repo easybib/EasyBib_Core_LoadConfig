d13a5588cb3ac55d9ea4e5bc23c9945f351e02ce
Anne-Julia Scheuermann <annejulia@gmail.com>
2016-06-07 18:01:36 +0200

    Travis: install extensions in environment

--- scripts/travis-deps.sh
+++ scripts/travis-deps.sh
@@ -9,14 +9,14 @@ install_ext_memcache() {
     wget "http://pecl.php.net/get/memcache-${MEMCACHE_VERSION}.tgz"
     tar -xzf "memcache-${MEMCACHE_VERSION}.tgz"
     sh -c "cd memcache-${MEMCACHE_VERSION} && phpize && ./configure --enable-memcache ${CONFIGURE_ARGS} && make && sudo make install"
-    echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+    echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 }
 
 install_ext_apc() {
     wget "http://pecl.php.net/get/APC-${APC_VERSION}.tgz"
     tar -xzf "APC-${APC_VERSION}.tgz"
     sh -c "cd APC-${APC_VERSION} && phpize && ./configure --enable-apc ${CONFIGURE_ARGS} && make && sudo make install"
-    echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+    echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 }
 
 install_ext_memcache
